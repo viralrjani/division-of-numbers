197f45a5accf69d93ce9c225a3c413180e236e1a
viralrjani <109897492+viralrjani@users.noreply.github.com>
2022-07-24 23:45:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ This app divide the 2 input number
 
 st.header('User Input Parameters')
 
-number1 = st.number_input('Number1',min_value=0.0,max_value=9999999.0)
-number2 = st.number_input('Number2',min_value=0.0,max_value=9999999.0)
+number1 = st.number_input('Number1')
+number2 = st.number_input('Number2')
 
 if number2 != 0:
   Ans = number1 / number2
