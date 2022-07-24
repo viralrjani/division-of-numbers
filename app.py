9b863e36c5c39f3b2e55c1b2f20cb322172487e4
viralrjani <109897492+viralrjani@users.noreply.github.com>
2022-07-24 18:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Division of 2 numbers
